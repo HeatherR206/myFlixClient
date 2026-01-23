6b0b4dff9640ac38f93bac70607861b209a8afc4
Heather <224328814+HeatherR206@users.noreply.github.com>
2026-01-22 17:39:12 -0800

    feat: implement useApi custom hook for centralized auth and fetching
    custom hook for use by all other React components

---
+++ src/hooks/useApi.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import { setUser } from "../redux/reducers/user";
+import { clearToken } from "../redux/reducers/token";
+
+export const useApi = () => {
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+
+    const authFetch = async (URL, options = {}) => {
+        const headers = {
+            ...options.headers,
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        };
+
+        try {
+            const response = await fetch(URL, { ...options, headers });
+
+            if (response.status === 401) {
+                alert("Session expired. Logging out.");
+                dispatch(setUser(null));
+                dispatch(clearToken());
+                localStorage.removeItem("user");
+                localStorage.removeItem("token");
+                return null;
+            }
+            return response;
+        } catch (error) {
+            console.error("Fetch error:", error);
+            throw error;
+        }
+    };
+    return { authFetch, user, token };
+};
